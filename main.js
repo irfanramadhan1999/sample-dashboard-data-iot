d4281c69b356b88e404a8070b9771266d8259bda
irfanramadhan1999 <irfanramadhani@students.amikom.ac.id>
2023-01-19 20:35:18 +0700

    update code

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   let table = document.getElementById('myTable');
+  
   $.ajax({
     url: "https://api-waterlevelmonitoring-production.up.railway.app/sensor",
     success: function (result) {
@@ -24,7 +25,7 @@ $(document).ready(function () {
             <td>${e["waterpump1"]}</td>
             <td>${e["waterpump2"]}</td>
             <td>${e["waktu"]}</td>
-            <td><button type="button" class="btn btn-primary" id="delete-button" onclick="deleteData(data.id)">Hapus</button></td>
+            <td><button type="button" class="btn btn-primary" id="delete-button" onclick="deleteData(${e['id']})">Hapus</button></td>
           </tr>
         `;
      });
@@ -34,3 +35,49 @@ $(document).ready(function () {
 });
 
 
+function deleteData(id) {
+   let table = document.getElementById("myTable");
+   let myAlert = document.getElementById("myAlert");
+   $.ajax({
+     url: "https://api-waterlevelmonitoring-production.up.railway.app/sensor/delete",
+     type: "POST",
+     data: { id: id },
+     success: function (result) {
+      myAlert.classList.add('show')
+       $.ajax({
+         url: "https://api-waterlevelmonitoring-production.up.railway.app/sensor",
+         success: function (result) {
+           let temp = `
+      <tr>
+            <th>id</th>
+            <th>Sensor 1</th>
+            <th>Sensor 2</th>
+            <th>Waterpump 1</th>
+            <th>Waterpump 2</th>
+            <th>Waktu</th>
+            <th>Hapus</th>
+      </tr>
+     `;
+
+           result.data.forEach((e) => {
+             temp += `
+           <tr>
+            <td>${e["id"]}</td>
+            <td>${e["sensor1"]}</td>
+            <td>${e["sensor2"]}</td>
+            <td>${e["waterpump1"]}</td>
+            <td>${e["waterpump2"]}</td>
+            <td>${e["waktu"]}</td>
+            <td><button type="button" class="btn btn-primary" id="delete-button" onclick="deleteData(${e["id"]})">Hapus</button></td>
+          </tr>
+        `;
+           });
+           table.innerHTML = temp;
+         },
+       });
+     },
+     error: function () {
+       console.log("gagal di hapus");
+     },
+   });
+}
